f7d078b2ca03e6d1754aecf4547f0cb40ca6c705
Atmiya Jadvani <atmiya@my.yorku.ca>
2023-10-21 14:21:42 -0400

    testing pull

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div id='pill'>
-        <p>Built by Team DevHealth</p>
+        <p style={{color: "black"}}>Built by Team DevHealth</p>
       </div>
     </div>
   );
