421ae0d83c37bc63aadfe8be32d79e99332bf6eb
Atmiya Jadvani <atmiya@my.yorku.ca>
2023-10-21 14:40:58 -0400

    Built the sign in screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Logo from './assets/images/Logo.svg';
+import GoogleButton from 'react-google-button'
 
 function App() {
   return (
     <div className="App">
       <div id='pill'>
-        <p style={{color: "black"}}>Built by Team DevHealth</p>
+        Built by Team DevHealth
+      </div>
+
+      <img style={{marginTop: "30px", width: "160px"}} src={Logo} alt="Logo" />
+      <h1 style={{fontSize: "38px", marginTop: "50px"}}>The Well-Being platform <br/>for Developers</h1>
+      <p style={{fontSize: "20px", marginTop: "-10px"}}>For developers to prioritize well-being through personalized health insights.</p>
+
+      <div className='App'>
+        {/* <button id='apple-login'>Sign in with Apple</button> */}
+        <GoogleButton style={{marginTop: "80px"}}
+          onClick={() => { console.log('Google button clicked') }}
+        />
+        <p>By continuing with Apple, you agree to DevHealth’s Term of Service and Privacy Policy</p>
       </div>
     </div>
   );
