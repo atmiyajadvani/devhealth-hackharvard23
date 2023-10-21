1c71e7f2756679f9702d15118e7622abc3d035b5
Min <minjo@bu.edu>
2023-10-21 16:44:27 -0400

    backend

--- backend/dbcon.js
+++ backend/dbcon.js
@@ -1,7 +1,7 @@
 //MongoDB conntect
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ojnim:6vrroBKDSAP7Hea9@hh23.godwcan.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ojnim:ZZY2A2p1fNyd6etg@hh23.godwcan.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
